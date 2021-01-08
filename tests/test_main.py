56f4904e307af66aef23b514b1e97b2e984ea1cf
Rahul.Bhave <rahul.bhave@qxf2.com>
2021-01-08 20:44:59 +0530

    first two tests
    added assertion for user_index
    
    update test case for message text check and asserting user,message index
    
    changed the doc strings for some of the tests
    
    first test training
    
    first test training
    
    polling training endpoint using asyncio
    
    removed test training file
    
    check for no two consecutive messages are same
    
    added test for checking uniqueness
    
    removed test for two consecutive messages
    
    assertion for first message
    
    made code more pythonic
    
    added check for first message for new cycle
    
    updated test with delete pickle file

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,9 +6,38 @@ import sys
 from fastapi.testclient import TestClient
 import main
 import messages
+import random
 from main import app
 from messages import reminders
+from messages import senior_qa_training
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+#Declaring directory
+REPO_DIR = os.path.dirname(os.path.dirname(__file__))
+MESSAGES_DIR = os.path.join(REPO_DIR, 'messages')
+
+# Declaring files to delete
+FILE_DELETE = ['senior_qa_training.pickle']
+
+# Delete file
+def delete_file(file_name):
+    """
+    This method will delete a particular file
+    """
+    if os.path.exists(file_name):
+        os.remove(file_name)
+        print(f'{file_name} deleted')
+
+# Delete files from particular directory
+def delete_files_in_dir(directory, files):
+    "The method will delete files in a particular directory"
+    for file_name in files:
+        delete_file(os.path.join(directory, file_name))
+
+# Delete pickle file
+def delete_pickle_file():
+    "The method will delete pickle file"
+    delete_files_in_dir(MESSAGES_DIR, FILE_DELETE)
 
 # Declaring test client
 client = TestClient(app)
@@ -71,3 +100,51 @@ def test_get_sep20_message_text_check():
     with open(main.SEP20_INTERNS_FILE, 'r') as file_handler:
         lines = [line.strip() for line in file_handler]
     assert message['msg'] in lines
+
+# Test for trainig status code
+def test_get_trainig():
+    "asserting status code"
+    response = client.get("/training")
+    assert response.status_code == 200
+
+# Test for asserting message with senior_qa_training messages
+def test_get_snior_qa_training_message_text_check():
+    "asserting message text check"
+    response = client.get("/training")
+    message = response.json()
+    assert response.json()["msg"] != ''
+    lines = senior_qa_training.messages
+    assert message['msg'] in lines
+
+# Test for asserting user_index and message_index are generated and incremented after every call
+def test_get_snior_qa_training_message():
+    "asserting user index and message_index generated and incremented after every call"
+    lines = senior_qa_training.messages
+    message = []
+    user_index_dict = []
+    message_index = 0
+    message = main.get_snior_qa_training_message("user")
+    assert message['msg'] in lines
+    user_index_dict = main.get_senior_qa_training_user_index()
+    message_index = user_index_dict['user'] + message_index
+    assert user_index_dict['user'] == message_index
+    main.set_senior_qa_training_user_index(user_index_dict)
+    assert user_index_dict['user'] == message_index
+    delete_pickle_file()
+
+# Test for asserting all messages are generated
+def test_unique_messages():
+    """
+    asserting unique messages are generated
+    """
+    messages = list()
+    for call_count in range(len(senior_qa_training.messages)):
+        message = main.get_snior_qa_training_message("user")
+        messages.append(message['msg'])
+    call_count += 1
+    assert call_count == len(senior_qa_training.messages)
+    assert len(set(messages)) == len(messages)
+    new_message = main.get_snior_qa_training_message("user")
+    assert messages[0] == new_message['msg']
+    print(f"\nFirst message of new cycle: {new_message['msg']}")
+    delete_pickle_file()
